9a268d8dd29e78acb3290e56f386e0ac340f0e83
Terence Wong <terencewong.work@gmail.com>
2023-07-08 16:14:38 -0500

    Update models.py

--- server/feedme/models.py
+++ server/feedme/models.py
@@ -1,10 +1,27 @@
 from django.db import models
+import uuid
 
 class User(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     email = models.EmailField()
+    reccomendations = models.ManyToManyField(Feed)
 
     def __str__(self):
         return self.username
+
+
+class Feed(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    content = models.TextField()  
+    title = models.CharField()
+    url = models.CharField()
+    date = models.DateField()
+    
+    class Meta:
+        ordering = ["date"]
+    
+    def __str__(self):
+        return self.title
 
